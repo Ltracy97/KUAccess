17d33e1d5d49a0d6dbf3e1452b797bc77c715d70
etoli2702 <123705864+etoli2702@users.noreply.github.com>
2023-04-15 12:28:09 -0500

    Add files via upload

--- email.js
+++ email.js
@@ -1,7 +1,4 @@
-const form = useRef();
-
-
-function check(){
+window.onload = function() {
     document.getElementById('report_form').addEventListener('submit', function(event) {
         event.preventDefault();
         
@@ -9,11 +6,13 @@ function check(){
             name: document.getElementById("name").value,
             email:document.getElementById("email").value,
             building:document.getElementById("building").value,
-            location:document.getElementByName("inorout").value,
-            description:document.getElementById("loc").value,
+            location:document.getElementById("inorout").value,
+            description:document.getElementById("loc").value
 
         }
-        emailjs.sendForm('service_0zquxn6', 'template_ufyro17', params)
+        this.test = params;
+
+        emailjs.sendForm('service_0zquxn6', 'template_ufyro17', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
